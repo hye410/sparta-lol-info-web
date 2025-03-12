b4f2b63f4b156eacd653e1773605cfcc15fd4f97
Hye10 <dhye410@daum.net>
2025-03-12 21:23:00 +0900

    [feat] 챔피언 목록 - 카드 클릭 시 디테일 페이지 링크 연결

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,8 @@
 import { BASE_IMAGE_URL } from "@/constants/api";
+import { PATH } from "@/constants/routes";
 import { Champion } from "@/types/champions";
 import Image from "next/image";
+import Link from "next/link";
 
 type CartProps = {
   data: Champion;
@@ -8,15 +10,17 @@ type CartProps = {
 
 export default function Card({ data }: CartProps) {
   return (
-    <section id="card">
-      <Image
-        src={`${BASE_IMAGE_URL}/champion/${data.image}`}
-        alt={data.name}
-        width={100}
-        height={100}
-      />
-      <h3 className="subTitle mt-4">{data.name}</h3>
-      <p className="text-default">{data.title}</p>
-    </section>
+    <Link href={`${PATH.CHAMPIONS}/${data.id}`}>
+      <section id="card">
+        <Image
+          src={`${BASE_IMAGE_URL}/champion/${data.image}`}
+          alt={data.name}
+          width={100}
+          height={100}
+        />
+        <h3 className="subTitle mt-4">{data.name}</h3>
+        <p className="text-default">{data.title}</p>
+      </section>
+    </Link>
   );
 }
