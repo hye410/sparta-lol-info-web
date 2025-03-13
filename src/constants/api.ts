ad50fbfb7898fef1e1f1445431e6ac04db4d4ced
Hye10 <dhye410@daum.net>
2025-03-13 12:28:47 +0900

    [chore] api constants 추가

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,5 +1,7 @@
-export const BASE_URL =
-  "https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR";
+const DEFAULT_URL = "https://ddragon.leagueoflegends.com/cdn/15.5.1";
 
-export const BASE_IMAGE_URL =
-  "https://ddragon.leagueoflegends.com/cdn/15.5.1/img";
+export const BASE_URL = `${DEFAULT_URL}/data/ko_KR`;
+
+export const BASE_IMAGE_URL = `${DEFAULT_URL}/img`;
+
+export const BASE_SPELL_URL = `${BASE_IMAGE_URL}/spell`;
