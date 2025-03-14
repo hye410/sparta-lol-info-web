c89131efc78f20436d4cb9e8c56d495a66b4b641
Hye10 <dhye410@daum.net>
2025-03-14 14:54:11 +0900

    [chore] className 오타 수정

--- src/app/rotation/page.tsx
+++ src/app/rotation/page.tsx
@@ -10,7 +10,7 @@ export default function Rotation() {
       <h1 className="title">
         챔피언 로테이션 (이번주 무료로 플레이 할 수 있어요!)
       </h1>
-      <div className="cardWrapper4">
+      <div className="cardWrapper">
         {champions?.map((champion) => (
           <Card key={`rotation_champion_${champion.id}`} data={champion} />
         ))}
