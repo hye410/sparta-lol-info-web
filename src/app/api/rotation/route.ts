ba516ea9f7ae2ded123e1fab7914ddc7a387231f
Hye10 <dhye410@daum.net>
2025-03-14 16:02:20 +0900

    [refactor] end point 요청 시 api 헤더에 포함시키는 것으로 수정

--- src/app/api/rotation/route.ts
+++ src/app/api/rotation/route.ts
@@ -4,9 +4,20 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    const res = await fetch(
-      `${ROTATION_BASE_URL}?api_key=${process.env.NEXT_PUBLIC_RIOT_API_KEY}`
-    );
+    const API_KEY = process.env.RIOT_API_KEY;
+    if (!API_KEY)
+      return new Response(JSON.stringify({ error: "API key is missing" }), {
+        status: 500,
+      });
+    const res = await fetch(ROTATION_BASE_URL, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Riot-Token": API_KEY,
+      },
+    });
+    if (!res.ok) throw new Error("failed to fetch champion rotation data");
+
     const { freeChampionIds } = await res.json();
     const { champions } = await getChampionsList();
     const rotationChampions = champions.filter(({ key }) => {
